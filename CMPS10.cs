4b9b2e9bfa002dcb2bf6f0bc0333db19e2a0bf7e
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-06-20 22:52:44 +0100

    Initial commit.

---
+++ CMPS10.cs
@@ -0,0 +1,49 @@
+﻿using Magellanic.I2C;
+using System;
+using System.Threading.Tasks;
+
+namespace Magellanic.Sensors.CMPS10
+{
+    public class CMPS10 : AbstractI2CDevice
+    {
+        private byte I2C_ADDRESS = 0x60;
+
+        public override byte[] GetDeviceId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte GetI2cAddress()
+        {
+            return I2C_ADDRESS;
+        }
+
+        public RawCompassData GetCompassData()
+        {
+            byte[] readBuffer = new byte[6];
+
+            this.Slave.WriteRead(new byte[] { 0x00 }, readBuffer);
+
+            return new RawCompassData(readBuffer);
+        }
+
+        public void RestoreFactorySettings()
+        {
+            this.Slave.Write(new byte[] { 0x16, 0x20 });
+            Task.Delay(100).Wait();
+            this.Slave.Write(new byte[] { 0x16, 0x2A });
+            Task.Delay(100).Wait();
+            this.Slave.Write(new byte[] { 0x16, 0x60 });
+        }
+
+        public void StartCalibration()
+        {
+            this.Slave.Write(new byte[] { 0x16, 0xF0 });
+        }
+
+        public void CalibrateOrdinalCompassPoint()
+        {
+            this.Slave.Write(new byte[] { 0x16, 0xF5 });
+        }
+    }
+}
---
+++ RawCompassData.cs
@@ -0,0 +1,28 @@
+﻿namespace Magellanic.Sensors.CMPS10
+{
+    public class RawCompassData
+    {
+        public RawCompassData(byte[] rawDeviceData)
+        {
+            this.SoftwareVersion = rawDeviceData[0];
+
+            this.BearingAsByte = rawDeviceData[1];
+
+            this.BearingAsWord = rawDeviceData[2] << 8 | rawDeviceData[3];
+
+            this.PitchAngle = rawDeviceData[4];
+
+            this.RollAngle = rawDeviceData[5];
+        }
+
+        public byte SoftwareVersion { get; private set; }
+
+        public byte BearingAsByte { get; private set; }
+
+        public int BearingAsWord { get; private set; }
+
+        public byte PitchAngle { get; private set; }
+
+        public byte RollAngle { get; private set; }
+    }
+}
